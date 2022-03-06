6cf83fae4e8320c126ce39a5118b97fe8601143c
Marquezv <viniciusmar2014@outlook.com>
2022-03-06 10:10:10 -0300

    create products page

--- public/js/product.js
+++ public/js/product.js
@@ -1,106 +1,85 @@
-import { cardProducts } from "./components.js"
+import { cardProducts, cardProductsDetails } from "./components.js"
+import { getProduct, getProductStore } from "./routes.js"
 
-const url = 'https://blx-app.herokuapp.com'
 document.addEventListener('DOMContentLoaded', function(){
    
     const product_id = sessionStorage.getItem('product_id')
     const user_id = sessionStorage.getItem('user_id')
     const token = sessionStorage.getItem('Authorization')
-    const name = sessionStorage.getItem('UserName')
-
-    const banner = document.getElementById('banner-text')
-    const navBarList = document.getElementById('navBarList')
 
     console.log(token)
-    if(token == null){
-      console.log('Deslogado')
-      
-      const text = '<h4>Faça o login ou registrese!</h4> <a href="login.html" class="btn btn-lg btn-primary">Login</a> <a href="register.html" class="btn btn-lg btn-primary">Register</a>'
-      banner.innerHTML = text
-      
-    }
-    else{
-      console.log('Logado')
-      
-      const navItem = '<li><a class="nav-link" href="index.html">Home</a></li><li><a class="nav-link" href="me.html">Me</a></li><li><a class="nav-link" href="pedidos.html">Order</a></li><li><button class="btn btn-danger" onclick="clearSession()" >Signout</button></li>'
-      text = `<h4>Bem Vindo, ${name}</h4>`
-      banner.innerHTML = text
-      navBarList.innerHTML = navItem
-  
-    }
-      
-    axios.get(`${url}/products/${product_id}`)
+
+    getProduct(product_id)
     .then(response => {
         const product = response.data
-
         const banner = document.getElementById('banner-text')
         const divCards = document.getElementById('div-product')
-
+        
         const text = `<h4>Nome da Loja: ${product.user.name}</h4>`
-        cardProducts(product)
-
-                
+        divCards.innerHTML = cardProductsDetails(product)
+        
         banner.innerHTML = text
-        divCards.innerHTML = card
 
 
     })
-    loadListProducts(user_id)
     
+    loadListProducts(user_id)
 })
 
 function loadListProducts(user_id){
-
-  axios.get(`${url}/products/store/${user_id}`)
+  getProductStore(user_id)
   .then(response => {
     const list = document.getElementById("list-products")
     const products = response.data
    
-    list.innerHTML = '';
-
-        products.forEach(products => {
-            cardProducts(products)
-            
-            const item = document.createElement('li')
-            item.innerHTML = productsList;
-        
-            list.appendChild(item)
-
-        });
+        list.innerHTML = products.map(products => {
+          return  cardProducts(products)
+        }).join('');
   })
 
 }
 
+function createOrder(){
+  const btn_addCart = document.getElementById('btn_addCart')
+  btn_addCart.onclick = (event) =>{
+      event.preventDefault()
+      
+         
+      
+       
+  }
+  
+}
 
-function createOrder(product_id){
-  const token = sessionStorage.getItem('Authorization')
-  axios.get(`${url}/products/${product_id}`)
-  .then(response => {
-    const pedido = response.data
+// function createOrder(product_id){
+//   const token = sessionStorage.getItem('Authorization')
+//   axios.get(`${url}/products/${product_id}`)
+//   .then(response => {
+//     const pedido = response.data
    
 
-    const CreateOrder = {
-      amount: 1,
-      delivery_place: pedido.product.delivery_place,
-      delivery_type: pedido.product.delivery_type,
-      notes: pedido.product.notes,
-      product_id: product_id
-    }
-      axios.post(`${url}/orders`, CreateOrder, {
-        headers: {
-            'Authorization' : `Bearer ${token}`
-        }
-      }).then(res => {
-        window.location.replace('pedidos.html')
+//     const CreateOrder = {
+//       amount: 1,
+//       delivery_place: pedido.product.delivery_place,
+//       delivery_type: pedido.product.delivery_type,
+//       notes: pedido.product.notes,
+//       product_id: product_id
+//     }
+//       axios.post(`${url}/orders`, CreateOrder, {
+//         headers: {
+//             'Authorization' : `Bearer ${token}`
+//         }
+//       }).then(res => {
+//         window.location.replace('pedidos.html')
         
-      })
+//       })
 
     
 
-  })
+//   })
   
 
-}
+// }
 
 // Compradores vendo 
 
